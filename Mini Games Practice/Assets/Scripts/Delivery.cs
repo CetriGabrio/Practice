55ebe7d3cb8028834b846a4d1230c54993f9d5af
Gabriele <gabriele.ilacqua@icloud.com>
2025-03-02 16:20:00 -0500

    Package system implemented

---
+++ Mini Games Practice/Assets/Scripts/Delivery.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage = false;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Bump");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Package")
+        {
+            Debug.Log("Package Picked Up");
+            hasPackage = true;
+        }
+
+        if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Package Delivered");
+            hasPackage = false;
+        }
+    }
+
+}
+ 
