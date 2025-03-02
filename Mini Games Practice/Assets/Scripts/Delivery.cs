80dd5530626adae1db26b64853c1fcf7cde5ec8a
Gabriele <gabriele.ilacqua@icloud.com>
2025-03-02 16:33:54 -0500

    Sprite Renderer to change car color

--- Mini Games Practice/Assets/Scripts/Delivery.cs
+++ Mini Games Practice/Assets/Scripts/Delivery.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage = false;
+
+    SpriteRenderer spriterenderer;
+
+    void Start()
+    {
+        spriterenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -13,16 +24,19 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package Picked Up");
             hasPackage = true;
+            spriterenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
 
         if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package Delivered");
             hasPackage = false;
+            spriterenderer.color = noPackageColor;
         }
     }
 
